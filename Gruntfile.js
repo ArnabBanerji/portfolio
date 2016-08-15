824f2f34469d9357865f78b2ca0532191d291ecb
Arnab Banerji <reach.arnab@live.in>
2016-08-16 04:59:04 +0530

    Lib concat added.

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Arnab Banerji on 16-08-2016.
+ */
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        concat: {
+            dist: {
+                // the files to concatenate
+                src: [
+                    'bower_components/jquery/dist/js/jquery.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js'
+                ],
+                // the location of the resulting JS file
+                dest: 'libs/libs.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat']);
+
+};
