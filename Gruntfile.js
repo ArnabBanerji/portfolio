ad7f170c59879afb5d8ba73ab81125687cb76ebc
Arnab Banerji <reach.arnab@live.in>
2016-08-18 02:13:02 +0530

    Lib concat added.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function (grunt) {
                 // the files to concatenate
                 src: [
                     'bower_components/jquery/dist/js/jquery.js',
-                    'bower_components/bootstrap/dist/js/bootstrap.js'
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'bower_components/angular/angular.js'
                 ],
                 // the location of the resulting JS file
                 dest: 'libs/libs.js'
